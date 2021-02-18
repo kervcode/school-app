63fe3380c82f6950d0555173809adc2b4bb7440c
Kervintz Noel <kervintz84@hotmail.com>
2021-02-17 22:59:15 -0500

    Updates

---
+++ precache-manifest.cae54d7f73b63c8b86f98a4885411a41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce05f1bbd8fac4eb03d547f1dd422682",
+    "url": "/school-app/index.html"
+  },
+  {
+    "revision": "94a9a58c0ec57ae5a488",
+    "url": "/school-app/static/css/main.e5a651a2.chunk.css"
+  },
+  {
+    "revision": "b610a256b3ecb70c8999",
+    "url": "/school-app/static/js/2.5fe30e26.chunk.js"
+  },
+  {
+    "revision": "94a9a58c0ec57ae5a488",
+    "url": "/school-app/static/js/main.c6ae229a.chunk.js"
+  },
+  {
+    "revision": "e6d2b44baf5252ce161e",
+    "url": "/school-app/static/js/runtime~main.d30b6eb1.js"
+  }
+]);
