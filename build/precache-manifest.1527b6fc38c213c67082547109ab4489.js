a18a9bb10beade873bb07649d7e63cddbd68b346
Kervintz Noel <kervintz84@hotmail.com>
2021-03-14 15:55:41 -0400

    merging

---
+++ build/precache-manifest.1527b6fc38c213c67082547109ab4489.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7aef8ddc131af68cdcbc1ce10474e8",
+    "url": "/school-app/index.html"
+  },
+  {
+    "revision": "0d9b47dda98cbc2042a2",
+    "url": "/school-app/static/css/main.e5a651a2.chunk.css"
+  },
+  {
+    "revision": "8b7e0499aea33a490e9c",
+    "url": "/school-app/static/js/2.5f1f276c.chunk.js"
+  },
+  {
+    "revision": "0d9b47dda98cbc2042a2",
+    "url": "/school-app/static/js/main.bb0a9498.chunk.js"
+  },
+  {
+    "revision": "e6d2b44baf5252ce161e",
+    "url": "/school-app/static/js/runtime~main.d30b6eb1.js"
+  }
+]);
